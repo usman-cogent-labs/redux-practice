612906b225bdad38788144c9cb68eed16e678681
usman-cogent-labs <109211422+usman-cogent-labs@users.noreply.github.com>
2023-01-16 12:18:22 +0500

    Code changed to redux toolkit

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,22 +1,25 @@
-import { createAction, createReducer } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
-export const bugAdded = createAction('bugAdded');
-export const bugRemoved = createAction('bugRemoved');
-export const bugResolved = createAction('bugResolved');
-
-createReducer([], {
-	[bugAdded.type]: (bugs, action) => {
-		bugs.push({
-			id: ++lastId,
-			description: action.payload.description,
-			resolved: false,
-		});
-	},
-	[bugRemoved]: (bugs, action) => {
-		bugs.filter((bug) => bug.id !== action.payload.id);
-	},
-	[bugResolved.type]: (bugs, action) => {
-		const index = bugs.findIndex((bug) => bug.id === action.payload.id);
-		bugs[index].resolved = true;
+let lastId = 0;
+const slice = createSlice({
+	name: 'bugs',
+	initialState: [],
+	reducers: {
+		bugAdded: (bugs, action) => {
+			bugs.push({
+				id: ++lastId,
+				description: action.payload.description,
+				resolved: false,
+			});
+		},
+		bugRemoved: (bugs, aciton) => {
+			bugs.filter((bug) => bug.id !== action.payload.id);
+		},
+		bugResolved: (bugs, action) => {
+			const index = bugs.findIndex((bug) => bug.id === action.payload.id);
+			bugs[index].resolved = true;
+		},
 	},
 });
+export const { bugAdded, bugRemoved, bugResolved } = slice.actions;
+export default slice.reducer;
